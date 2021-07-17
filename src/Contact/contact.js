3f1a7078f9ede55136a4a5e62fbf38d80bdc25e4
soubhagya23 <ranjansoubhagya.master@gmail.com>
2021-07-17 11:01:14 +0530

    Initial Deployment

---
+++ src/Contact/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./contact.css"
+import Image from "../Assets/TajMahal.jpg"
+
+export default function contact(){
+return(
+   
+   <div className="main_heading">
+       <h1>Contact Us</h1>
+   </div>
+   
+)
+}
